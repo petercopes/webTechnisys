08d3014052068b7dc5e3143ccacda9982b853c96
Pedro Copes <petercopes.96@gmail.com>
2021-11-16 17:03:17 -0300

    added user input and local storage

--- js/welcome.js
+++ js/welcome.js
@@ -1,14 +1,32 @@
 let welcomeText = document.getElementById('welcomeText');
-function requestUserEmail(){
-    const userEmail = prompt('Welcome to Le Homme! Please insert your email to subscribe and get a 10% discount code or leave it blank.');
-    alert(`Valor ingresado: ${userEmail}`);
-    return userEmail;
-}
-document.addEventListener('DOMContentLoaded',()=>{
-    const subscriberEmail  = requestUserEmail();
-    const userArr = subscriberEmail.split('@');
-    const username = userArr[0];
-    if(subscriberEmail && userArr.length>1){
-        welcomeText.innerText= `Welcome ${username} to LeHomme! Check your email for your coupon code`
+const modal = document.getElementById('welcomeModal');
+const form = document.getElementById('welcomeForm');
+const closebutton = document.getElementById('closeLogo');
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const formdata = new FormData(form);
+    const email = formdata.get('email');
+    if(email){
+        console.log(email)
+        localStorage.setItem('email',email);
+        modal.classList.remove('welcomeModalActive');
     }
+});
+closebutton.addEventListener('click',()=>{
+    modal.classList.remove('welcomeModalActive');
 })
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const email = localStorage.getItem('email');
+    if(!email){
+        modal.classList.add('welcomeModalActive');
+    }
+    else{
+        const userArr = email.split('@');
+        const username = userArr[0];
+        if(email && userArr.length>1){
+            welcomeText.innerText= `Welcome ${username} to LeHomme! Check your email for your coupon code`
+        }
+    }
+});
+
