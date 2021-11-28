776f6c930fc7b7daf3a0cc8cdf75fc760d93a075
Pedro Copes <petercopes.96@gmail.com>
2021-11-28 13:25:45 -0300

    fixed productitem id

--- js/productos.js
+++ js/productos.js
@@ -1,29 +1,28 @@
-
 const container = document.getElementById("productsContainer");
 
-function createProductDiv (product) {
-  let productDiv = document.createElement('div');
+function createProductDiv(product) {
+  let productDiv = document.createElement("div");
   productDiv.classList.add("col-sm-12");
-  productDiv.classList.add('col-md-6');
-  productDiv.classList.add('productItem');
-  productDiv.id = `producto${product.prodId}`;
-  let link = document.createElement('a');
-  link.setAttribute('href','product/index.html');
-  let image = document.createElement('img');
-  image.setAttribute('src',product.imageSrc);
-  image.classList.add('productImage');
+  productDiv.classList.add("col-md-6");
+  productDiv.classList.add("productItem");
+  productDiv.id = `producto-${product.codigoProducto}`;
+  let link = document.createElement("a");
+  link.setAttribute("href", "product/index.html");
+  let image = document.createElement("img");
+  image.setAttribute("src", product.imageSrc);
+  image.classList.add("productImage");
   link.appendChild(image);
-  let productInfoDiv = document.createElement('div');
-  productInfoDiv.classList.add('flex');
-  productInfoDiv.classList.add('productInfo'); 
-  let titleLink = document.createElement('a');
-  titleLink.setAttribute('href','product/index.html');
-  let title = document.createElement('h3');
+  let productInfoDiv = document.createElement("div");
+  productInfoDiv.classList.add("flex");
+  productInfoDiv.classList.add("productInfo");
+  let titleLink = document.createElement("a");
+  titleLink.setAttribute("href", "product/index.html");
+  let title = document.createElement("h3");
   title.innerText = product.name;
   titleLink.appendChild(title);
-  let price = document.createElement('p');
-  price.innerText= product.price;
-  price.classList.add('price');
+  let price = document.createElement("p");
+  price.innerText = product.price;
+  price.classList.add("price");
   productInfoDiv.appendChild(titleLink);
   productInfoDiv.appendChild(price);
   productDiv.appendChild(link);
@@ -31,11 +30,12 @@ function createProductDiv (product) {
   return productDiv;
 }
 
-$('document').ready(async() => {
-  const productos = await $.get('https://60d8bcffeec56d00174774a8.mockapi.io/productos');
+$("document").ready(async () => {
+  const productos = await $.get(
+    "https://60d8bcffeec56d00174774a8.mockapi.io/productos"
+  );
   for (const product of productos) {
     productDiv = createProductDiv(product);
     container.appendChild(productDiv);
-    
   }
 });
